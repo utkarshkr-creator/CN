72f8a88eb59ed6327bdd531c78fe97a859f1366e
utkarsh kumar <66742842+utkarshkr-creator@users.noreply.github.com>
2022-07-25 10:06:36 +0530

    Create server.c

---
+++ Chat Server/server.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<string.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+void error(const char *msg){
+	perror(msg);   
+	exit(1);  
+}
+int main(int argc,char *argv[]){
+	if(argc<2){
+		fprintf(stderr,"Port number not found.Program Terminated.\n");     
+		exit(1);   
+	}
+	int sockfd,newsockfd,portno,n;    
+	char buffer[255];   
+	struct sockaddr_in serv_addr,cli_addr;   
+	socklen_t clilen;   
+        sockfd=socket(AF_INET,SOCK_STREAM,0);    
+	if(sockfd<0)
+		error("ERROR in Socket\n");    
+        bzero((char *) &serv_addr,sizeof(serv_addr));   
+	portno=atoi(argv[1]);     
+   	serv_addr.sin_family=AF_INET;
+	serv_addr.sin_addr.s_addr=INADDR_ANY;   
+	serv_addr.sin_port=htons(portno);
+	if(bind(sockfd,(struct sockaddr*)&serv_addr,sizeof serv_addr)<0){
+		error("BINDING Failded.\n");  
+	}
+	listen(sockfd,5);   
+	clilen=sizeof cli_addr;   
+	newsockfd=accept(sockfd,(struct sockaddr *)&cli_addr,&clilen);   
+	if(newsockfd<0)error("Accept Faild.\n");    
+	while(1){
+		bzero(buffer,255);    
+		n=read(newsockfd,buffer,255);   
+		if(n<0)error("Read Failded.\n");   
+		printf("Client: %s\n",buffer);    
+		bzero(buffer,255);   
+		fgets(buffer,255,stdin);    
+		n=write(newsockfd,buffer,strlen(buffer));   
+		if(n<0)
+			error("Write Failded.\n");   
+		int i=strncmp("Bye",buffer,3);   
+		if(i==0)break;
+	}
+	return 0; 
+}
